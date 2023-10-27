2a47289f52b0325a58e35046a76c6889cd4f4a3a
Daphne Calin <daphnecalin@gmail.com>
2023-10-26 21:15:02 -0400

    fix password storage

--- main.py
+++ main.py
@@ -7,24 +7,25 @@ def encoder(pw):
 
 def main():
     choice = ""
+    password_data = ""
     while choice != "3":
         print("1. Encode")
         print("2. Decode")
         print("3. Quit")
         choice = input("Please enter an option: ")
 
-        password_data = ""
         if choice == "1":
             password = input("Please enter your password to encode: ")
             try: 
                 password_data = encoder(password)
             except:
-                print("Invalid password, must be numbers only")
+                print("Invalid password")
             finally:
                 print("Your password has been encoded and stored!")
+                print(password_data)
         elif choice == "2":
             try:
-                print(f"The encoded password is {password_data}, and the original password is {password}") # needs decoder function to be implemented
+                print(f"The encoded password is {password_data}, and the original password is {decoder(password_data)}") # needs decoder function to be implemented
             except:
                 print("Invalid input, must be numbers only")
 
